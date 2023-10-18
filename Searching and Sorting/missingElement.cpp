cfdf9c39d2d6ab7fe4db41088d0bfd7c2e4fccc0
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-10-18 15:43:32 +0530

    Update missingElement.cpp

--- Searching and Sorting/missingElement.cpp
+++ Searching and Sorting/missingElement.cpp
@@ -12,3 +12,17 @@ public:
     }
 };
 
+//o(n)
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       int sum = 0; 
+       for(int i =0 ; i<=nums.size();i++){
+           sum+=i;
+       }
+       for(int i = 0 ;i<nums.size();i++){
+           sum-=nums[i];
+       }
+       return sum;
+    }
+};
