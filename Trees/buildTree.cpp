4b254fb9671ad0c83721ebc964af54c5b53a34fd
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-10-20 23:23:54 +0530

    Create buildTree.cpp

---
+++ Trees/buildTree.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+        Node(int data){
+            this->data = data;
+            left= NULL;
+            right=NULL;
+        }
+};
+Node* buildTree(){
+    int data ; 
+    // cout<<"Enter the root Node";
+    cin>>data;
+    if(data==-1){
+        return NULL;
+    }
+    Node* root = new Node(data);
+    cout<<"Enter the leftNode of"<<data<<": ";
+    root->left = buildTree();
+    cout<<"Enter the rightNode of"<<data<<": ";
+    root->right = buildTree();
+}
+int main(){
+    Node* root = buildTree();
+}
