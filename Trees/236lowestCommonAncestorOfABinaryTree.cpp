3d30382005f271b00140a64af5c159c7f534758f
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-11-04 23:10:28 +0530

    Create 236lowestCommonAncestorOfABinaryTree.cpp

---
+++ Trees/236lowestCommonAncestorOfABinaryTree.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->val==p->val){
+            return p;
+        }
+        if(root->val==q->val){
+            return q;
+        }
+        TreeNode* leftAns = lowestCommonAncestor(root->left , p , q);
+        TreeNode* rightAns = lowestCommonAncestor(root->right , p , q);
+        if(leftAns==NULL && rightAns!=NULL){
+            return rightAns;
+        }else if(leftAns!=NULL && rightAns==NULL){
+            return leftAns;
+        }else if(leftAns!=NULL && rightAns!=NULL){
+            return root;
+        }else{
+            return NULL;
+        }
+
+    }
+};
