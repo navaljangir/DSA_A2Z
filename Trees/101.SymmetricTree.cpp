cafe48026665fcef11918bbe21be78bc764fabb9
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-11-04 16:05:35 +0530

    Create 101.SymmetricTree.cpp

---
+++ Trees/101.SymmetricTree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool solve(TreeNode* p , TreeNode* q){
+        if(p==NULL && q==NULL){
+            return true;
+        }
+        if((p!=NULL && q==NULL) || (q!=NULL && p==NULL)){
+            return false;
+        }
+        if(p->val !=q->val){
+            return false;
+        }
+        return solve(p->left , q->right) && solve(p->right, q->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return solve(root->left, root->right);
+    }
+};
