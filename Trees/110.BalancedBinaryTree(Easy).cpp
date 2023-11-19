de3774f619f00a8b5c4858f18d207c4855c715ce
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-11-19 10:33:05 +0530

    Update and rename 110.BalancedBinaryTree.cpp to 110.BalancedBinaryTree(Easy).cpp

--- Trees/110.BalancedBinaryTree(Easy).cpp
+++ Trees/110.BalancedBinaryTree(Easy).cpp
@@ -34,3 +34,24 @@ public:
         return rootAns&&leftAns&&rightAns;
     }
 };
+
+//Optimised O(n)
+class Solution {
+public:
+    bool isbalanced = true;
+    int height(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        int lh = height(root->left);
+        int rh = height(root->right);
+        if(abs(lh-rh)>1){
+            isbalanced =false;
+        }
+        return max(lh , rh )+1;
+    }
+    bool isBalanced(TreeNode* root) {
+        height(root);
+        return isbalanced;
+    }
+};
