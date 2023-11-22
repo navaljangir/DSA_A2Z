d18183b21c5ac2db772ca0deb9a7238d7c027814
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-11-22 15:21:05 +0530

    Create 235.lowestCommonAncestorOfBinarySearchTree(Medium).cpp

---
+++ binarySearchTree/235.lowestCommonAncestorOfBinarySearchTree(Medium).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    TreeNode* solve(TreeNode* root , TreeNode* p , TreeNode* q){
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->val>p->val && root->val>q->val){
+            return solve(root->left ,p , q);
+        }
+        if(root->val<p->val && root->val<q->val){
+            return solve(root->right , p ,q);
+        }
+        return root;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return solve(root , p , q);
+    }
+};
