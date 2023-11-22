6a7f36dca940676df9988effaa834cebbb79a6b3
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-11-22 12:08:06 +0530

    Update 98.ValidateABinarySearchTree(Medium).cpp

--- binarySearchTree/98.ValidateABinarySearchTree(Medium).cpp
+++ binarySearchTree/98.ValidateABinarySearchTree(Medium).cpp
@@ -18,3 +18,27 @@ public:
         return isValid(root , lb , up);
     }
 };
+
+
+//Using inorder traversal Concept (As we know in inorder traversal of binart search tree(bst) values are in sorted order)
+class Solution {
+public:
+    void isValid(TreeNode* root,vector<long long>& v){
+       if(root==NULL){
+           return;
+       }
+       isValid(root->left, v);
+       v.push_back(root->val);
+       isValid(root->right , v);
+    }
+    bool isValidBST(TreeNode* root) {
+        vector<long long> v;
+        isValid(root , v);
+        for(int i =1 ;i<v.size();i++){
+            if(v[i]<=v[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
