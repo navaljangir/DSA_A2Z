715cb3e297773d5e32e6aadcb55fa637479e44bc
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-11-22 11:19:53 +0530

    Create 450.DeleteANodeInABST(medium).cpp

---
+++ binarySearchTree/450.DeleteANodeInABST(medium).cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maxValue(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        if(root->right==NULL){
+            return root->val;
+        }
+        return maxValue(root->right);
+    }
+    TreeNode* solve(TreeNode* root , int key){
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->val==key){
+            if(root->left==NULL && root->right==NULL){
+                delete root;
+                return NULL;
+            }else if(root->left!=NULL && root->right==NULL){
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }else if(root->left==NULL && root->right!=NULL){
+                TreeNode*temp = root->right;
+                delete root;
+                return temp;
+            }else{
+                int findMaxRoot= maxValue(root->left);
+                root->val = findMaxRoot;
+                root->left = solve(root->left , findMaxRoot);
+            }
+        }else if(root->val<key){
+            root->right = solve(root->right , key);
+        }else{
+            root->left = solve(root->left , key);
+        }
+        return root;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+     return solve(root , key);   
+    }
+};
