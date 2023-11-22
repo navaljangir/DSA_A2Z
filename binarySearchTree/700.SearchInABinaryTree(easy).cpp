bfa6347de0f992023da3e1e600cb7e8a01237d98
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-11-22 09:45:02 +0530

    Update 700.SearchInABinaryTree(easy).cpp

--- binarySearchTree/700.SearchInABinaryTree(easy).cpp
+++ binarySearchTree/700.SearchInABinaryTree(easy).cpp
@@ -1,3 +1,25 @@
+//O(logn)
+class Solution {
+public:
+    // TreeNode* ans = NULL;
+    TreeNode* solve(TreeNode* root , int val){
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->val==val){
+            return root;
+        }
+        if(val<root->val){
+            return solve(root->left , val);
+        }else{
+            return solve(root->right , val);
+        }
+    }
+    TreeNode* searchBST(TreeNode* root, int val) {
+        return solve(root , val);
+    }
+};
+//O(n)
 class Solution {
 public:
     TreeNode* ans = NULL;
