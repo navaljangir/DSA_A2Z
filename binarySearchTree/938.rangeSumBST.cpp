f1a5519c3d8b2e76e8ef3caa4d777abe58033488
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-12-02 11:53:35 +0530

    Update 938.rangeSumBST.cpp

--- binarySearchTree/938.rangeSumBST.cpp
+++ binarySearchTree/938.rangeSumBST.cpp
@@ -1,3 +1,34 @@
+//Morris traversal
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        TreeNode* curr=root;
+        int ans=0;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                if(curr->val>=low && curr->val<=high)
+                ans+=curr->val;
+                curr=curr->right;
+            }else{
+                TreeNode* pred = curr->left;
+                while(pred->right!=curr && pred->right!=NULL){
+                    pred=pred->right;
+                }
+                if(pred->right==NULL){
+                    pred->right=curr;
+                    curr=curr->left;
+                }else{
+                     if(curr->val>=low && curr->val<=high)
+                ans+=curr->val;
+                    pred->right=NULL;
+                    curr=curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
+//Recursion
 class Solution {
 public:
     void solve(TreeNode*root, int low ,int high,int&ans){
