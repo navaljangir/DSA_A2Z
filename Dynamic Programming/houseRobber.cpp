86cf812cba6256505874a6118709a567b90c81b7
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-12-13 15:38:17 +0530

    Update houseRobber.cpp

--- Dynamic Programming/houseRobber.cpp
+++ Dynamic Programming/houseRobber.cpp
@@ -17,9 +17,9 @@ public:
         return solvemem(nums , 0, -1 );
     }
 };
-
-
-//top down 
+/*-------------------------------------------------------------------------------------more optimised  ---------------------------------------------------------------------------------*/
+    
+//top down - 2d dp
 class Solution {
 public:
     int solvemem(vector<int>& nums , int i , int prev ,vector<vector<int>>&dp){
@@ -40,5 +40,28 @@ public:
     int rob(vector<int>& nums) {
         vector<vector<int>> dp(nums.size() , vector<int>(nums.size(),-1));
         return solvemem(nums , 0, -1 ,dp);
+    }
+};
+
+
+//1d dp
+class Solution {
+public:
+    int solvemem(vector<int>& nums , int i  ,vector<int>&dp){
+        if(i>=nums.size()){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int take = 0 ;
+        int notTake = 0;
+        take = nums[i] + solvemem(nums , i+2 ,dp);
+        notTake = solvemem(nums , i+1 , dp);
+        return dp[i] = max(take , notTake);
+    }
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(),-1);
+        return solvemem(nums , 0, dp);
     }
 };
