2396af7c01b7f0bc60b91410362513fe594702b2
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-12-18 16:14:26 +0530

    Update knapsackProblem.cpp

--- Dynamic Programming/knapsackProblem.cpp
+++ Dynamic Programming/knapsackProblem.cpp
@@ -68,27 +68,58 @@ class Solution
 class Solution
 {
     public:
+    //Function to return max value that can be put in knapsack of capacity W.
     int knapsackMaxSum(int capacity, int wt[] , int val[] , int n ){
-        vector<vector<int>> dp(n , vector<int>(capacity+1, 0));
+        vector<int> prev(capacity+1 , 0);
+        vector<int> curr(capacity+1 , 0 );
         for(int i = wt[0]; i<=capacity; i++){
-            dp[0][i] = val[0];
+            prev[i]= val[0];
         }
         for(int i =1 ; i <n; i++){
             for(int j = 0 ; j <=capacity;j++){
                 int include =0;
                 if(wt[i] <=j){
-                    include = val[i] + dp[i-1][j-wt[i]];
+                    include = val[i] + prev[j-wt[i]];
                 }
-               int exclude = dp[i-1][j];
-                dp[i][j] = max(include ,exclude);
+               int exclude =prev[j];
+                curr[j]= max(include ,exclude);
             }
+            prev = curr;
         }
-        return dp[n-1][capacity];
+        return prev[capacity];
     }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-       // Your code here
-       vector<vector<int>> dp(n , vector<int>(W+1, -1));
+       return knapsackMaxSum(W , wt, val ,n);
+      
+    }
+};
+
+
+//More Space optimisation O(capacity)
+class Solution
+{
+    public:
+    int knapsackMaxSum(int capacity, int wt[] , int val[] , int n ){
+        vector<int> curr(capacity+1 , 0 );
+        for(int i = wt[0]; i<=capacity; i++){
+            curr[i]= val[0];
+        }
+        for(int i =1 ; i <n; i++){
+            for(int j = capacity ; j>=0;j--){
+                int include =0;
+                if(wt[i] <=j){
+                    include = val[i] + curr[j-wt[i]];
+                }
+               int exclude =curr[j];
+                curr[j]= max(include ,exclude);
+            }
+            // prev = curr;
+        }
+        return curr[capacity];
+    }
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
        return knapsackMaxSum(W , wt, val ,n);
       
     }
