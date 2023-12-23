88b0623567b3801bf4a8d2ef364aecac49a9832c
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-12-23 09:59:52 +0530

    Update 300.longestIncreasingSubsequence.cpp

--- Dynamic Programming/300.longestIncreasingSubsequence.cpp
+++ Dynamic Programming/300.longestIncreasingSubsequence.cpp
@@ -22,3 +22,51 @@ public:
         return solve(0, nums, -1 ,dp);
     }
 };
+
+//Tabulation
+class Solution {
+public:
+    int solveTab(vector<int>&nums){
+        vector<vector<int>> dp(nums.size() +1 , vector<int>(nums.size()+1 , 0));
+        for(int i =nums.size()-1; i>=0;i--){
+            for(int j =i-1 ; j>=-1;j--){
+                int include =0 ; 
+                if(j==-1 || nums[i]>nums[j]){
+                    include = 1+dp[i+1][i+1];
+                }
+                int exclude= dp[i+1][j+1];
+                 dp[i][j+1]= max(include , exclude);
+                    }
+                }
+                return dp[0][0];
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        return solveTab(nums);
+    }
+};
+
+
+//DP with binary search 
+class Solution {
+public:
+    int solveOptimal(vector<int>&nums){
+        if(nums.size()==0){
+            return 0;
+        }
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        for(int i = 0; i <nums.size();i++){
+            if(nums[i]>ans.back()){
+                ans.push_back(nums[i]);
+            }else{
+                int index = lower_bound(ans.begin() , ans.end() , nums[i]) - ans.begin();
+                ans[index] = nums[i];
+            }
+        }
+        return ans.size();
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        return solveOptimal(nums);
+    }
+};
+
