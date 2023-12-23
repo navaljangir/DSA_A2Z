fe52fb49a6cf84ba6882496acdc15bd3447c08b1
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2023-12-23 11:32:52 +0530

    Update 1691.maximumHeightByStackingCuboids(hard).cpp

--- Dynamic Programming/1691.maximumHeightByStackingCuboids(hard).cpp
+++ Dynamic Programming/1691.maximumHeightByStackingCuboids(hard).cpp
@@ -1,4 +1,4 @@
-//DP - top down
+//DP - top down 
 class Solution {
 public:
     int solve(vector<vector<int>>& cuboids , int index , int prev ,vector<vector<int>>&dp){
@@ -22,6 +22,62 @@ public:
         vector<vector<int>> dp(cuboids.size()+1, vector<int>(cuboids.size()+1 , -1));
         sort(cuboids.begin() , cuboids.end());
         return solve(cuboids , 0, -1, dp);
+
+    }
+};
+
+//DP - Tab 
+class Solution {
+public:
+    int solveTab(vector<vector<int>>& cuboids){
+        vector<vector<int>> dp(cuboids.size()+1, vector<int>(cuboids.size()+1 , 0));
+        for(int index = cuboids.size()-1; index>=0;index--){
+            for(int prev = index-1; prev>=-1 ;prev--){
+                int include = 0;
+            if(prev==-1 || (cuboids[index][0]>=cuboids[prev][0] && cuboids[index][1]>=cuboids[prev][1] && cuboids[index][2]>=cuboids[prev][2])){
+                include = cuboids[index][2] + dp[index+1][index+1];
+            }
+            int exclude = dp[index+1][prev+1];
+                dp[index][prev+1]  =max(include, exclude);
+                }
+            }
+            return dp[0][0];
+    }
+    int maxHeight(vector<vector<int>>& cuboids) {
+        for(int i =0 ; i <cuboids.size();i++){
+            sort(cuboids[i].begin() , cuboids[i].end());
+        }
+        sort(cuboids.begin() , cuboids.end());
+        return solveTab(cuboids);
+
+    }
+};
+
+//DP - space optimised
+class Solution {
+public:
+    int solveTab(vector<vector<int>>& cuboids){
+        vector<int> next(cuboids.size()+1);
+        vector<int> curr(cuboids.size()+1);
+        for(int index = cuboids.size()-1; index>=0;index--){
+            for(int prev = index-1; prev>=-1 ;prev--){
+                int include = 0;
+            if(prev==-1 || (cuboids[index][0]>=cuboids[prev][0] && cuboids[index][1]>=cuboids[prev][1] && cuboids[index][2]>=cuboids[prev][2])){
+                include = cuboids[index][2] + next[index+1];
+            }
+            int exclude = next[prev+1];
+                curr[prev+1]  =max(include, exclude);
+                }
+                next= curr;
+            }
+            return next[0];
+    }
+    int maxHeight(vector<vector<int>>& cuboids) {
+        for(int i =0 ; i <cuboids.size();i++){
+            sort(cuboids[i].begin() , cuboids[i].end());
+        }
+        sort(cuboids.begin() , cuboids.end());
+        return solveTab(cuboids);
 
     }
 };
