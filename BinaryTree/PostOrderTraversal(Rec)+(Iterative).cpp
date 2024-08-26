ca963803426772bebe1491ca7a879d0f297af51a
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2024-08-26 16:50:31 +0530

    Update PostOrderTraversal(Rec)+(Iterative).cpp

--- BinaryTree/PostOrderTraversal(Rec)+(Iterative).cpp
+++ BinaryTree/PostOrderTraversal(Rec)+(Iterative).cpp
@@ -30,17 +30,6 @@ public:
 
 //Iterative using 2 stacks
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
@@ -66,6 +55,43 @@ public:
             int top =st2.top();
             st2.pop();
             ans.push_back(top);
+        }
+        return ans;
+    }
+};
+
+
+
+//Iterative using 1 stacks
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(root==NULL){
+            return {};
+        }
+        stack<TreeNode*> st;
+        TreeNode* curr =root;
+        vector<int> ans;
+        while(curr!=NULL || !st.empty()){
+            if(curr!=NULL){
+                st.push(curr);
+                curr = curr->left;
+            }else{
+                TreeNode* temp = st.top()->right;
+                if(temp==NULL){
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && st.top()->right==temp){
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }else{
+                    curr = temp;
+                }
+            }
         }
         return ans;
     }
