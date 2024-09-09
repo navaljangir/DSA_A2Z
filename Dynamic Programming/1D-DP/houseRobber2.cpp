00b691cfe2d2e75ace0551939c0cb9d32f771fea
Naval Kishor <104446367+navaljangir@users.noreply.github.com>
2024-09-09 14:40:11 +0530

    Update houseRobber2.cpp

--- Dynamic Programming/1D-DP/houseRobber2.cpp
+++ Dynamic Programming/1D-DP/houseRobber2.cpp
@@ -24,3 +24,51 @@ public:
         return max(ans1 , ans2);
     }
 };
+
+
+//Tabulation 
+class Solution {
+public:
+    int solveTab(vector<int>&nums , int s , int e){
+        vector<int> dp(e-s+3, -1);
+        dp[e] =0; 
+        dp[e+1] = 0 ; 
+        for(int i = e-1; i>=s; i--){
+            dp[i] = max({dp[i] ,dp[i+1] , nums[i] + dp[i+2]});
+        }
+        return dp[s];
+    }
+    int rob(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
+        int n = nums.size();
+        int ans1 = solveTab(nums , 0 , n- 1 );
+        int ans2 = solveTab(nums , 1 , n); 
+        return max(ans1 , ans2);
+    }
+};
+
+//Space optimized 
+class Solution {
+public:
+    int solveTab(vector<int>&nums , int s , int e){
+        int next1 =0 ;
+        int next2 =0 ;
+        for(int i = e-1; i>=s; i--){
+            int curr = max({next1 , nums[i] + next2});
+            next2 = next1;
+            next1 = curr;
+        }
+        return next1;
+    }
+    int rob(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
+        int n = nums.size();
+        int ans1 = solveTab(nums , 0 , n- 1 );
+        int ans2 = solveTab(nums , 1 , n); 
+        return max(ans1 , ans2);
+    }
+};
